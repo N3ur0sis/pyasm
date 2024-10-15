21849cea53eb36a1b444b425b83a23a6317c24e3
baptiste pachot <baptistepachot@gmail.com>
2024-10-15 13:54:10 +0200

    ajout exemple 1

---
+++ build/CMakeFiles/pyasm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyasm.dir/src/main.cpp.o"
+  "CMakeFiles/pyasm.dir/src/main.cpp.o.d"
+  "pyasm"
+  "pyasm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pyasm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
