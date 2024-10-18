0c465088dcb1b1ce98e5c5dc33909ac0744bf2c2
baptiste pachot <baptistepachot@gmail.com>
2024-10-18 17:37:55 +0200

    modif print

--- src/main.cpp
+++ src/main.cpp
@@ -33,37 +33,43 @@ int main(int argc, char* argv[]){
     std::vector<Token> tokens = lexer.tokenize();
 
     for(auto &[type, value] : tokens) {
-        if(type == TokenType::INTEGER) {
+    switch(type) {
+        case TokenType::INTEGER:
             std::cout << "Type : INTEGER" << ", Value :" << value << std::endl;
-        }
-        if(type == TokenType::KW_RETURN) {
-            std::cout << "Type : RETURN"  << ", Value :" << value << std::endl;
-        }
-        if(type == TokenType::OP_PLUS) {
-            std::cout << "Type : PLUS" << ", Value :" << value  << std::endl;
-        }
-        if(type == TokenType::OP_MOD) {
-            std::cout << "Type : MOD" << ", Value :" << value  << std::endl;
-        }
-        if(type == TokenType::OP_MUL) {
-            std::cout << "Type : MUL" << ", Value :" << value  << std::endl;
-        }
-        if(type == TokenType::OP_MOINS) {
-            std::cout << "Type : MUL" << ", Value :" << value  << std::endl;
-        }
-        if(type == TokenType::IDF) {
-            std::cout << "Type : IDF"  << ", Value :" << value << std::endl;
-        }
-        if(type == TokenType::NEWLINE) {
-            std::cout << "Type : NEWLINE"  << ", Value :" << "NULL" << std::endl;
-        }
-        if(type == TokenType::BEGIN) {
-            std::cout << "Type : BEGIN"  << ", Value :" << "NULL" << std::endl;
-        }
-        if(type == TokenType::END) {
-            std::cout << "Type : END"  << ", Value :" << "NULL" << std::endl;
-        }
+            break;
+        case TokenType::KW_RETURN:
+            std::cout << "Type : RETURN" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::OP_PLUS:
+            std::cout << "Type : PLUS" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::OP_MOD:
+            std::cout << "Type : MOD" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::OP_MUL:
+            std::cout << "Type : MUL" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::OP_MOINS:
+            std::cout << "Type : MOINS" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::IDF:
+            std::cout << "Type : IDF" << ", Value :" << value << std::endl;
+            break;
+        case TokenType::NEWLINE:
+            std::cout << "Type : NEWLINE" << ", Value : NULL" << std::endl;
+            break;
+        case TokenType::BEGIN:
+            std::cout << "Type : BEGIN" << ", Value : NULL" << std::endl;
+            break;
+        case TokenType::END:
+            std::cout << "Type : END" << ", Value : NULL" << std::endl;
+            break;
+        default:
+            std::cout << "Type : UNKNOWN" << ", Value :" << value << std::endl;
+            break;
     }
+}
+
 
   return 0;
   }
